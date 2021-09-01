6c3ffce3340198053a30f09f3921af64e46acdc8
Taniya Sterling <sterlingtaniya@gmail.com>
2021-08-31 21:16:35 -0400

    working on seeding

---
+++ db/seed.js
@@ -0,0 +1,58 @@
+
+const {
+  client,
+  getAllUsers
+} = require('./index');
+
+
+async function testDB() {
+  try {
+
+    client.connect();
+    const users = await getAllUsers();
+    console.log(users);
+    console.log(rows);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
+}
+
+testDB();
+
+async function dropTables() {
+  try {
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+    `);
+  } catch (error) {
+    throw error;
+  }
+}
+
+// this function should call a query which creates all tables for our database 
+async function createTables() {
+  try {
+    await client.query(`
+
+    `);
+  } catch (error) {
+    throw error; // we pass the error up to the function that calls createTables
+  }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
+}
+
+rebuildDB();
